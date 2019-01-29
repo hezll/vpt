a8f29bdc87f3a8ce7c0b7d4a506bf45fd292c1da
david.zheng <david.zhang@team.telstra.com>
2019-01-29 23:48:24 +1100

    MYKI Page layout

--- vpt_app/common/js/constant.js
+++ vpt_app/common/js/constant.js
@@ -1,6 +1,6 @@
 //url相关
-const BASE_URL = "http://47.75.7.246:8080";
-//const BASE_URL = "http://localhost:8080"
+//const BASE_URL = "https://vpt.hjaustralia.com";
+const BASE_URL = "http://localhost:8080"
 
 //登录
 const DEPARTURE_URL = BASE_URL + "/api/ptv/departures/";
@@ -10,6 +10,8 @@ const DIRECTION_URL = BASE_URL + "/api/ptv/initDirections";
 const STOP_NAME_MAP_URL = BASE_URL+"/api/ptv/initStopReverse";
 const ROUTE_STOP_MAP_URL = BASE_URL + "/api/ptv/initRouteStop";
 const DISRUPTION_URL = BASE_URL + "/api/ptv/disruptions";
+
+const MYKI_CARDS_URL = BASE_URL + "/api/ptv/myki/getCards";
 
 const HEADERS = {
   'content-type': 'application/json',
@@ -26,5 +28,6 @@ module.exports = {
 	DIRECTION_URL: DIRECTION_URL,
 	ROUTE_STOP_MAP_URL: ROUTE_STOP_MAP_URL,
 	DISRUPTION_URL: DISRUPTION_URL,
-  HEADERS: HEADERS,
+	MYKI_CARDS_URL: MYKI_CARDS_URL,
+	HEADERS: HEADERS,
 }
--- vpt_app/pages/myki/myki.vue
+++ vpt_app/pages/myki/myki.vue
@@ -1,8 +1,38 @@
 <template>
-	<view>
+	<view v-if="loginPage" class="loginPage">
+		<view class="logo">
+			<view>
+				<image src="../../static/images/myki_logo.jpeg" mode="aspectFit" class="logoimg"></image>
+			</view>
+			<view class="tip">
+				To see your account balance, log in to your myki online account
+			</view>
+		</view>
+		<view class="content">
+			<view style="flex-direction:column;width:100%;">
+				<view class=" has-mgtb-10 ">
+					<input type="number" maxlength="11" v-model="login.phone" placeholder="myki username" class="is-input1 " @input="BindInput"
+					 data-val="phone" />
+				</view>
+				<view class="has-mgtb-20">
+					<input v-model="login.password" placeholder="myki password" class="is-input1" @input="BindInput" data-val="password" />
+				</view>
+
+				<view class="loginbtn has-mgtb-30">
+					<button :loading="login.loading" @tap="defaultHandlerLogin"> {{ login.loading ? "Login...":"Login"}} </button>
+				</view>
+			</view>
+		</view>
+		<view>
+			<navigator url="#" hover-class="" style="font-size:32upx;padding-top: 20upx;">
+				<text>I accept the </text><text class="is-blue"> Privacy Statement</text>
+			</navigator>
+		</view>
+	</view>
+	<view v-else style="flex:1;position:fixed;bottom:0;width:100%;">
 		<page-head :title="title"></page-head>
-		<view class="example">
-			<view class="example-title">左侧滑出</view>
+		<view class="mykiList">
+			<!-- <view class="example-title">左侧滑出</view>
 			<view>
 				<button type="default" @click="show('left')">显示Drawer</button>
 				<uni-drawer :visible="showLeft" mode="left" @close="closeDrawer('left')">
@@ -16,55 +46,89 @@
 						<button type="default" @click="hide">关闭Drawer</button>
 					</view>
 				</uni-drawer>
+			</view> -->
+			<view class="card" v-for="(item,index) in cards" :key="index">
+				<view>
+					<image src="../../static/images/myki_logo.jpeg" style="border-radius:50px;height:35px;width:35px;" />
+				</view>
+				<view style="flex-direction: column;">
+					<view style="font-weight: bold;">{{item.cardNum}}</view>
+					<view style="padding-top: 18upx;">{{item.holder}}</view>
+				</view>
+				<view style="flex-direction: column;align-items:flex-end">
+					<view style="font-size:28upx">Available</view>
+					<view style="font-weight: bold;">{{item.mykiMoney}}</view>
+				</view>
 			</view>
 		</view>
 	</view>
 </template>
 <script>
-	import uniDrawer from "@/components/uni-drawer/uni-drawer.vue"
+	import net from '@/common/js/netUtil.js'
+	import con from '@/common/js/constant.js'
 
 	export default {
-		components: {
-			uniDrawer
-		},
 		data() {
 			return {
-				showRigth: false,
-				showLeft: false,
-				title: 'Drawer'
+				cards: [],
+				loginPage: true,
+				username: '',
+				password: '',
+				login: {
+					loading: false,
+					phone: "",
+					password: ""
+				},
+
 			}
 		},
 
 		onLoad: function() {
+			if (uni.getStorageSync("mykiLogin")) {
+				this.loginPage = false;
+				this.cards = uni.getStorageSync("cards");
+				this.loadingCards();
+			} else {
+				this.loginPage = true;
+			}
 			uni.setNavigationBarColor({
 				frontColor: '#ffffff',
-				backgroundColor: '#bed600',
-			})
-			uni.setNavigationBarTitle({
-				title: 'myki'
+				backgroundColor: '#c4da2d',
 			})
 		},
-		methods: {
 
-			show(e) {
-				console.info(e);
-				if (e === 'left') {
-					this.showLeft = true
-				} else {
-					this.showRigth = true
+		onPullDownRefresh() {
+			this.loadingCards(uni.stopPullDownRefresh());
+		},
+
+		methods: {
+			loadingCards() {
+				let body = {
+					username: 'david1022',
+					password: '2lg5lcgv'
 				}
+				net.netUtil(con.MYKI_CARDS_URL, 'GET', body, ret => {
+					if (ret) {
+						this.cards = ret;
+						uni.setStorage({
+							key: "cards",
+							data: ret
+						})
+					}
+				});
 			},
-			hide() {
-				this.showLeft = false
-				this.showRigth = false
+			defaultHandlerLogin: function() {
+				this.login.loading = true;
+				setTimeout((e => {
+					this.login.loading = false;
+					this.loginPage = false;
+				}), 1500);
+				
 			},
-			closeDrawer(e) {
-				if (e === 'left') {
-					this.showLeft = false
-				} else {
-					this.showRigth = false
-				}
-			}
+			BindInput: function(e) {
+				var dataval = e.currentTarget.dataset.val;
+				this.login[dataval] = e.detail.value;
+			},
 		},
 		onBackPress() {
 			if (this.showRigth || this.showLeft) {
@@ -76,6 +140,124 @@
 </script>
 
 <style>
+	page {
+		/* height: 100%; */
+		background-color: #ececec;
+	}
+
+	.loginPage {
+		flex-direction: column;
+		width: 100%;
+		align-items: center;
+	}
+
+	.logo {
+		margin-top: 80rpx;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.content {
+		width: 85%;
+		margin: 0 auto;
+	}
+
+	.tip {
+		color: #6a6d73;
+		line-height: 40upx;
+		width: 85%;
+		font-size: 30upx;
+		font-weight: 500;
+		padding: 40upx 0;
+	}
+
+	.has-mgtb-10 {
+		margin-bottom: 10rpx;
+	}
+
+	.has-mgtb-20 {
+		margin-bottom: 20rpx;
+	}
+
+	.has-mgtb-20 {
+		margin-bottom: 30rpx;
+	}
+
+	.loginbtn button {
+		margin-top: 20rpx;
+		height: 88rpx;
+		width: 100%;
+		line-height: 88rpx;
+		color: #333839;
+		font-size: 42rpx;
+		border-radius: 44rpx;
+		outline: 0;
+		display: block;
+		margin: 0;
+		font-family: inherit;
+		background: #bed600;
+		opacity: 0.8;
+		font-weight: bold;
+	}
+
+	button:after {
+		border: 2rpx solid #f2f2f2;
+	}
+
+	.logoimg {
+		width: 200rpx;
+		height: 200rpx;
+		border-radius: 50%;
+	}
+
+	.is-blue {
+		color: #2A62FF !important;
+	}
+
+	.is-input1 {
+		height: 88rpx;
+		width: 100%;
+		line-height: 88rpx;
+		padding: 12rpx;
+		color: #353535;
+		font-size: 32rpx;
+		box-sizing: border-box;
+		appearance: none;
+		border: 2rpx solid #e5e5e5;
+		box-shadow: none;
+		border-radius: 44rpx;
+		outline: 0;
+		display: block;
+		padding-left: 30rpx;
+		margin: 0;
+		font-family: inherit;
+		background: #fff;
+		resize: none;
+	}
+
+
+	.mykiList {
+		justify-content: flex-end;
+		flex: 1;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.card {
+		color: white;
+		/* background-color: #F78248; */
+		border-top-right-radius: 10px;
+		border-top-left-radius: 10px;
+		height: 170upx;
+		width: 95%;
+		flex-direction: row;
+		justify-content: space-around;
+		padding-top: 15px;
+		margin-bottom: -10px;
+		background: linear-gradient(orange 0%,
+			#F78248 30%);
+	}
+
 	.close {
 		padding: 30upx;
 	}
